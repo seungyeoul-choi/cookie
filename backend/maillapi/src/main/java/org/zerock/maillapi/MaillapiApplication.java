4092ba5f23e62c51d81810634596fa790321264f
seungyeoul-choi <seungyeoul@gmail.com>
2026-03-10 18:59:04 +0900

    Initial commit: cookie project

---
+++ backend/maillapi/src/main/java/org/zerock/maillapi/MaillapiApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.maillapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaillapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaillapiApplication.class, args);
+	}
+
+}
---
+++ backend/maillapi/src/test/java/org/zerock/maillapi/MaillapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.maillapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaillapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
